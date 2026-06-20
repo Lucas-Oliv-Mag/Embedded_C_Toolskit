a27ea2c27a3a8cf41d8c94c28198e1fad9175eba
Lucas <Lucasmagalhaess2240@gmail.com>
2026-06-19 22:37:46 -0300

    Fix: padding matters behavior improviment
    Now the pop and push functions return how many data was really manipuled in the Lenght_in_bytes parameters, turning this parameter a pointer

--- Ring buffer/README.md
+++ Ring buffer/README.md
@@ -3,14 +3,30 @@
 Usefull for bare-metal envioriments, with deterministic behavior
 and flexibility for any kind of data.
 
+_can be utillized for build anothers data sctruct algorithms like Queues._
+
 
 ## Standard APIs
  * **Init** - Initialize the ring buffer handler (mandatory first step).
+ * **Config** - Configue the behavior of the funcions when edge cases occurs.
  * **Push** - insert data into a buffer ring.
  * **Pop**  - read and exclude data from ring buffer.
  * **Peek** - read data without pop them.
  * **Avaliable** - return how many bytes in use now.
   
+## warnings to avoid undefined behaviors
+
+  - Have certain of the behavior of the ring buffer api's when they are next to full with the
+    function _ringbf_config()_.
+
+  - Only change the size of the buffer or any variable in the circular buffer handler until you  have a certain that no others function ill be executed, especially if the Optimized version is enabled.
+  
+  - Always check if the value of the parameter _Lenght_in_bytes_ changed after a pop or 
+    push call, it means that your requisition partial occurs.
+
+  - Don't try push in a the buffer data large than they lenght.
+
+
 ## Optimized version
 
  ### when using **#define _RB_OPTIMIZED_VERSION**, a power of two optimization with binary masks will be enable (be careful, the buffer size must match with power of two, or all optimized functions will return a error in wich return).
--- Ring buffer/src/ringbuffer.c
+++ Ring buffer/src/ringbuffer.c
@@ -23,12 +23,9 @@ enum rb_msg ringbf_init(struct ring_buffer * ptr, const uint16_t max_size, const
   ptr->lock = true;
 
   #ifdef _RB_OPTIMIZED_VERSION // optimized version only works with buffer sizes that are a power of 2, because it uses bitwise specificaly operations.
-    if(!_RB_POWER_OF_2_CHECK(ptr->size)){ ptr->lock = false; return RB_BASE_2_ERR; }
+    if(!_RB_POWER_OF_2_CHECK(max_size)){ ptr->lock = false; return RB_BASE_2_ERR; }
   #endif
   
-
-  ptr->lock = true;
-
   ptr->size = max_size;
   ptr->buffer = (uint8_t *)buffer;
   ptr->tail = 0;
@@ -81,21 +78,30 @@ enum rb_msg ringbf_clear(struct ring_buffer * ptr){
 
 
 
-enum rb_msg ringbf_push(struct ring_buffer * ptr, const void * data_in, uint16_t lenght_in_bytes){
+enum rb_msg ringbf_push(struct ring_buffer * ptr, const void * data_in, uint16_t * lenght_in_bytes){
 
-  if(ptr == NULL || lenght_in_bytes == 0) return RB_PARAM_ERR;
+  if(ptr == NULL || lenght_in_bytes == NULL || data_in == NULL){ return RB_PARAM_ERR; }
+  if(*lenght_in_bytes > ptr->size || *lenght_in_bytes == 0){ return RB_PARAM_ERR; }
 
   _RB_LOCK_();
 
+  if(ptr->overwrite == false && (*lenght_in_bytes + ptr->counter) > ptr->size){
 
-  if(ptr->overwrite == false && (lenght_in_bytes + ptr->counter) > ptr->size){
+    if(ptr->padding_matters == true){
+      
+      ptr->lock = false;
+      return RB_WITHOUT_SPACE;
+       
+    }else{
 
-    ptr->lock = false;
-    return RB_WITHOUT_SPACE;
+      *lenght_in_bytes = ptr->size - ptr->counter; // adjust the push size to fit the remaining space.
 
-  }else if(ptr->overwrite == true && (lenght_in_bytes + ptr->counter) > ptr->size){
+    }
+    
 
-    uint16_t bytes_to_overwrite = (lenght_in_bytes + ptr->counter) - ptr->size;
+  }else if(ptr->overwrite == true && (*lenght_in_bytes + ptr->counter) > ptr->size){
+
+    uint16_t bytes_to_overwrite = (*lenght_in_bytes + ptr->counter) - ptr->size;
 
     #ifdef _RB_OPTIMIZED_VERSION
       ptr->head = (ptr->head + bytes_to_overwrite) & (ptr->size - 1); // optimized wrap-around using bitwise AND for power of 2 sizes.
@@ -107,7 +113,7 @@ enum rb_msg ringbf_push(struct ring_buffer * ptr, const void * data_in, uint16_t
 
   } 
 
-  for(uint16_t index = 0U; index < lenght_in_bytes; index++){
+  for(uint16_t index = 0; index < *lenght_in_bytes; index++){
   
       ptr->buffer[ ptr->tail ] = ((uint8_t *)data_in)[index];    
       #ifdef _RB_OPTIMIZED_VERSION
@@ -118,7 +124,7 @@ enum rb_msg ringbf_push(struct ring_buffer * ptr, const void * data_in, uint16_t
 
   }
 
-  ptr->counter = (ptr->counter + lenght_in_bytes > ptr->size) ? ptr->size : ptr->counter + lenght_in_bytes;
+  ptr->counter = (ptr->counter + *lenght_in_bytes > ptr->size) ? ptr->size : ptr->counter + *lenght_in_bytes;
 
   ptr->lock = false;
   
@@ -128,43 +134,45 @@ enum rb_msg ringbf_push(struct ring_buffer * ptr, const void * data_in, uint16_t
 
 
 
-enum rb_msg ringbf_pop(struct ring_buffer * ptr, void * data_out, uint16_t lenght_in_bytes){
+enum rb_msg ringbf_pop(struct ring_buffer * ptr, void * data_out, uint16_t* lenght_in_bytes){
 
 
-  if(ptr == NULL || lenght_in_bytes == 0 || data_out == NULL){ return RB_PARAM_ERR; }
+  if(ptr == NULL || lenght_in_bytes == NULL || data_out == NULL){ return RB_PARAM_ERR; }
   
+  if(*lenght_in_bytes > ptr->size || *lenght_in_bytes == 0){ return RB_PARAM_ERR; }
+
+  if(ptr->counter == 0){ return RB_EMPY_ERR; }
+
   _RB_LOCK_();
-
-
 
   uint16_t pops_to_do = 0;
   bool lacking_flag = false;
 
-  if(lenght_in_bytes > ptr->counter){
+  if(*lenght_in_bytes > ptr->counter){
 
     if(ptr->padding_matters == true){
       
       ptr->lock = false;
-      rb_critical_exit();
 
       return RB_BLK_LACKING;
        
     }
     else{
 
-      pops_to_do = ptr->counter;
+      *lenght_in_bytes = ptr->counter;
       lacking_flag = true;
        
     }
-
-  }else{ pops_to_do = lenght_in_bytes; }
+  }
+    
+  pops_to_do = *lenght_in_bytes; 
 
   for(uint16_t index = 0U; index < pops_to_do; index++){
 
    *((uint8_t *)(((uint8_t*)data_out) + index)) = (ptr->buffer[ptr->head]);
     #ifdef _RB_OPTIMIZED_VERSION
       ptr->head = (ptr->head + 1) & (ptr->size - 1); // optimized wrap-around using bitwise AND for power of 2 sizes.
-    #else 
+    #else
       ptr->head = (ptr->head + 1) % ptr->size;
     #endif
   }
